f170ca0d874096c2546b8fcf2dc6d4cd6ad6a579
Harounjlassi <jlassi.Haroun@esprit.tn>
2025-10-03 13:58:01 +0100

    update: custom challenge components

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,50 @@
+import { useState, useRef, useEffect } from "react";
+
 export default function Player() {
+  const playerName = useRef();
+
+  const [name, setName] = useState(null);
+  // const [submit, setSubmit] = useState(false);
+
+  //  useEffect(() => {
+  //   console.log("submit changed:", submit);
+  // }, [submit]);
+  // function handleChange(e) {
+  //   setSubmit(false)
+  //   setName(e.target.value);
+
+
+  // }
+  function handleClick() {
+    //setSubmit(true)
+    //console.log(submit)// <-- logs false
+
+    // Why does it log false?
+    // setSubmit(true) does not update submit immediately.
+    // In React, setState is asynchronous: React schedules a re-render with the new value, but it doesn’t mutate the variable submit right away.
+    // So inside the same function call, submit still holds its old value (false).
+    // After the function finishes, React will re-run the component, and then submit will be true.
+
+
+
+    console.log(playerName.current.type)
+    setName(playerName.current.value);
+    playerName.current.value='';
+
+
+  }
+  //console.log(submit)// <-- logs true
+  //console.log("dddddddddddd")
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      {/* <h2>Welcome {playerName ? playerName : 'unknown'} entity</h2> */}
+      <h2>Welcome {name ?? 'unknown'} entity</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        {/* ref is special prop */}
+        {/* <input ref={playerName} type="text" value={name} onchgange /> */}
+        <input ref={playerName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,20 @@
+export default function TimerChallenge({ title, targetTime }) {
+    return (
+        <section className="challenge">
+            <h2>{title}</h2>
+            <p className="challenge-time">
+                {targetTime} second{targetTime > 1 ? 's' : ''}
+
+            </p>
+            <p>
+                <button>
+                    Strat/ Stop Challenge
+                </button>
+            </p>
+            <p className="">
+                Time is running.../ Time inactive
+            </p>
+        </section>
+    )
+
+}
